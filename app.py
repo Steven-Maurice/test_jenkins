d38260534812d308b35c4fab59e9e0cd120446cc
Steven M <100772049+Steven-Maurice@users.noreply.github.com>
2024-02-07 21:25:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
-""" import Flask """
-from flask import Flask, jsonify
+'''
+    fichier main
+''' 
 
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    """ main function"""
-    return jsonify(message="Hello, World!")
-
-if __name__ == '__main__':
-    app.run(debug=True)
+print('hello from gitlab')
